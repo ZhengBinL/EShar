a07ecfe1b7e8dcf5e453e727de4b52566212008d
zhengbinlong <774037817@qq.com>
2019-04-12 11:05:42 +0800

    font-css修改

--- src/pages/home/daytime.vue
+++ src/pages/home/daytime.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="daytime">
-        daytime
+        <div class="head">
+            <mt-header title="请选择日期">
+                <router-link to="/" slot="left">
+                    <mt-button icon="back"></mt-button>
+                </router-link>
+                <mt-button icon="more" slot="right"></mt-button>
+            </mt-header>
+        </div>
     </div>
 </template>
 
@@ -10,6 +17,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.search{
+    height: 50px;
+}
 </style>
--- src/pages/home/search.vue
+++ src/pages/home/search.vue
@@ -1,15 +1,51 @@
 <template>
     <div class="search">
-        search
+        <div class="head">
+            <mt-header title="搜索">
+                <router-link to="/" slot="left">
+                    <mt-button icon="back"></mt-button>
+                </router-link>
+                <mt-button icon="more" slot="right"></mt-button>
+            </mt-header>
+        </div>
+        <div class="search">
+            <mt-search v-model="value" cancel-text="取消" placeholder="中文/拼音/首字母" autofocus :result="filterResult">
+            </mt-search>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <i class="iconfont icon-shijian"></i><span>搜索历史</span>
+                </div>
+                <div>
+                    <i class="iconfont icon-shanchu"></i><span>清空</span>
+                </div>
+            </div>
+            <div>
+                <span>同利大酒店</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            citySearch:[],
+            value:''
+        }
+    },
+    computed:{
+        filterResult(){
+            return this.citySearch.filter(value => new RegExp(this.value, 'i').test(value))
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.search{
+    height: 50px;
+}
 </style>
